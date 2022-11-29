d6b349ed2a824b55c8027c5b4c37024ad565c738
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-11-29 23:11:29 +0200

    Add files via upload

---
+++ Old-Exams/GamesPlay/src/api/users.js
@@ -0,0 +1,33 @@
+import { clearUserData, setUserData } from "../util.js";
+import { get, post } from "./api.js";
+
+export async function login(email,password){
+const res = await post('/users/login', {email, password})
+const userData = {
+    id: res._id,
+    email: res.email,
+    accessToken: res.accessToken
+}
+ setUserData(userData)
+
+
+return res
+
+}
+
+export async function register(email,password){
+ const res = await post('/users/register', {email, password})
+ const userData = {
+    id: res._id,
+    email: res.email,
+    accessToken: res.accessToken
+}
+ setUserData(userData)
+ return res
+}
+export  function logout(){
+get('/users/logout');
+clearUserData()
+
+
+}
